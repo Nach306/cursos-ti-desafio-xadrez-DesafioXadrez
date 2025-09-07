49059502f8cf0d6996196f8dcfae280789c991a4
Nach306 <nachalipedroso@gmail.com>
2025-09-07 18:14:36 +0000

    Vrsao 2.0

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
+// o Desafio de Xadrez - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
